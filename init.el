8d75b36692c81e19959286f9ef80eaa2ba398943
Nigel Owen <nigelowen2@gmail.com>
2025-09-06 21:43:37 +1200

    tided it up a bit, got it working ok under c

--- init.el
+++ init.el
@@ -1,198 +1,176 @@
-(require 'package)                                          ; activate packagesnix-channel --list
-'(package-archives
-  '(("gnu" . "https://elpa.gnu.org/packages/")
-    ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-    ("melpa" . "https://melpa.org/packages/")))
+;; Initialize package system
+(require 'package)
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")
+        ("gnu-devel" . "https://elpa.gnu.org/devel/")))
 
+;; Start server if not running
+(load "server")
+(unless (server-running-p) (server-start))
 
-;;(package-initialize)                                        ; initialize package facility
-
-
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-
-(add-to-list 'package-archives '("gnu-devel" . "https://elpa.gnu.org/devel/"))
-
-
+;; Install use-package if not present
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Configure use-package
+(eval-when-compile
+  (require 'use-package))
 
+;; UI Configuration
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+
+;; Turn off UI elements
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; History and auto-revert
+(setq history-length 25)
+(savehist-mode 1)
+(global-auto-revert-mode 1)
+
+;; Line numbers
+(global-display-line-numbers-mode 1)
+
+;; Theme
+;;(load-theme 'ef-trio-dark t)
+(load-theme 'leuven-dark t)
+
+
+;; Package configuration
 (use-package web-mode
   :ensure t)
 
+(use-package treesit-auto
+  :ensure t
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
 
-;;;;;;;;;;;;;;;;;;; some c stuff which I just chucked in, hopefully good :)
+;; Font locking
+(setq font-lock-highlight-level 4)
+(setq treesit-font-lock-level 4)
+(setq font-lock-maximum-decoration 4)
 
-(use-package eglot)
-
+;; LSP and completion
+;; (use-package eglot
+;;   :ensure t
+;;   :config
+;;   (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+;;   (add-hook 'c-mode-hook 'eglot-ensure)
+;;   (add-hook 'c++-mode-hook 'eglot-ensure))
 
 (use-package lsp-mode
   :ensure t
   :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :config (setq lsp-completion-enable-additional-text-edit nil))
+  :config
+  (setq lsp-completion-enable-additional-text-edit nil))
+
 (use-package lsp-ui
-  :commands lsp-ui-mode
-  :ensure t)
-;; (use-package ccls
-;;   :ensure t
-;;   :config
-;;   (setq ccls-executable "ccls")
-;;   (setq lsp-prefer-flymake nil)
-;;   (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
-;;   :hook ((c-mode c++-mode objc-mode) .
-;;          (lambda () (require 'ccls) (lsp))))
-(use-package flycheck
-  :ensure t)
+  :ensure t
+  :commands lsp-ui-mode)
+
+(use-package company
+  :ensure t
+  :config
+  (global-company-mode))
+
 (use-package yasnippet
   :ensure t
-  :config (yas-global-mode))
+  :config
+  (yas-global-mode))
+
 (use-package which-key
   :ensure t
-  :config (which-key-mode))
+  :config
+  (which-key-mode))
+
 (use-package helm-lsp
   :ensure t)
-(use-package helm
-  :ensure t
-  :config (helm-mode))
+
 (use-package lsp-treemacs
   :ensure t)
 
+;; Programming modes
+(use-package go-mode
+  :ensure t
+  :hook (go-mode . (lambda ()
+                     (eglot-ensure)
+                     (setq tab-width 4)
+                     (add-hook 'before-save-hook #'eglot-format-buffer -10 t))))
 
-;;; This will enable emacs to compile a simple cpp single file without any makefile by just pressing [f9] key
-(defun code-compile()
+(use-package rust-mode
+  :ensure t
+  :hook (rust-mode . (lambda ()
+                       (setq indent-tabs-mode nil)
+                       (prettify-symbols-mode))))
+
+(use-package lua-mode
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.script\\'" . lua-mode)))
+
+;; Compilation
+(defun code-compile ()
   (interactive)
   (unless (file-exists-p "Makefile")
     (set (make-local-variable 'compile-command)
-	 (let ((file (file-name-nondirectory buffer-file-name)))
-	   (format "%s -o %s %s"
-		   (if (equal (file-name-extension file) "cpp") "g++" "gcc")
-		   (file-name-sans-extension file)
-		   file)))
+         (let ((file (file-name-nondirectory buffer-file-name)))
+           (format "%s -o %s %s"
+                   (if (equal (file-name-extension file) "cpp") "g++" "gcc")
+                   (file-name-sans-extension file)
+                   file)))
     (compile compile-command)))
+
 (global-set-key [f9] 'code-compile)
 
+;; General settings
+(setq scheme-program-name "racket")
+(setq-default dired-listing-switches "-alh")
+(setq-default tab-width 4)
+(setq tab-always-indent 'complete)
+(add-to-list 'completion-styles 'initials t)
 
-(require 'eglot)
-(add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
-(add-hook 'c-mode-hook 'eglot-ensure)
-(add-hook 'c++-mode-hook 'eglot-ensure)
+ (which-key-mode)
+ (add-hook 'c-mode-hook 'lsp)
 
-(use-package company :ensure t)
+(require 'lsp-ui)
+(setq lsp-ui-sideline-enable t)
+(setq lsp-ui-sideline-show-hover nil)
+(setq lsp-ui-sideline-show-code-actions nil)
+(setq lsp-ui-sideline-position 'right) ; This puts it on the right side
+(lsp-ui-sideline-mode t)
+(put 'scroll-left 'disabled nil)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; End of stuff which may be good
+;; Let's run 8 checks at once instead.
+(setq flymake-max-parallel-syntax-checks 8)
+;; Prefer minibuffer for documentation
 
-;;; golang
+;; Completely disable hover functionality
+;(setq eglot-extra-doc-mode nil)
+;(add-to-list 'eglot-ignored-server-capabilities :hoverProvider)
 
-(require 'company)
-(require 'yasnippet)
+(setq gdb-debuginfod-enable-setting nil)
+(setq gdb-many-windows t)
+(setq gdb-show-main t)
+;(setq company-tooltip-limit 10)          ; Show 10 candidates max
 
-(require 'go-mode)
-(require 'eglot)
-(add-hook 'go-mode-hook 'eglot-ensure)
-
-;; Optional: install eglot-format-buffer as a save hook.
-;; The depth of -10 places this before eglot's willSave notification,
-;; so that that notification reports the actual contents that will be saved.
-(defun eglot-format-buffer-before-save ()
-  (add-hook 'before-save-hook #'eglot-format-buffer -10 t))
-(add-hook 'go-mode-hook #'eglot-format-buffer-before-save)
-;;
-
-
-
- ;;(guix-emacs-autoload-packages)
-
- ;; in theory hide the title bar
- ;;(add-to-list 'default-frame-alist '(undecorated . t))
-
- (setq my-packages
-       '(go-mode lua-mode modus-themes ef-themes magit catppuccin-theme lsp-mode)
-       )
-
- (setq-default dired-listing-switches "-alh")
-
-;; TBD - 
- ;; (unless package-archive-contents    ; unless packages are not available locally, dont refresh package archives
- ;;   (package-refresh-contents))       ; refreshing package contents is time-consuming and should be done on demand
-
- (dolist (pkg my-packages)          
-   (unless (package-installed-p pkg)
-     (package-install pkg)))        
-
- (setq scheme-program-name "racket")
-
- (setq inhibit-startup-message t)  ; Comment at end of line!
- (setq ring-bell-function 'ignore)
-
- ;; Turn off some unneeded UI elements
- ;;(menu-bar-mode -1)
- (tool-bar-mode -1)
- (scroll-bar-mode -1)
- ;; Save what you enter into minibuffer prompts
- (setq history-length 25)
- (savehist-mode 1)
- ;; Revert buffers when the underlying file has changed
- (global-auto-revert-mode 1)
- ;; Revert buffers when the underlying file has changed
- (global-auto-revert-mode 1)
-
- ;; Display line numbers in every buffer
- (global-display-line-numbers-mode 1)
-
- ;;(setq modus-themes-common-palette-overrides '((border-mode-line-active
- ;;    unspecified) (border-mode-line-inactive unspecified)))
-
- (setq catppuccin-flavor 'mocha) ;; or 'latte, 'macchiato, or 'mocha
-
-
- ;; Load the Modus Vivendi dark theme
-					;(load-theme 'modus-vivendi t)
- (load-theme 'catppuccin :no-confirm)
-
-
-
- (add-to-list 'auto-mode-alist '("\\.script\\'" . lua-mode))
-
- (setq-default tab-width 4)
-
-
- (defun my-set-tab-mode ()
-   (when (and (stringp buffer-file-name)
-	      (string-match "\\.cat\\'" buffer-file-name))
-     (insert "OK")
-     (orgtbl-mode)))
-
- (add-hook 'find-file-hook 'my-set-tab-mode)
-
- (setq tab-always-indent 'complete)
- (add-to-list 'completion-styles 'initials t)
-
- ;; go Stuff
-
- ;; (require 'lsp-mode)
- ;; (require 'go-mode)
-
- ;; (add-hook 'go-mode-hook 'lsp-deferred)
- ;; (add-hook 'go-mode-hook 'subword-mode)
- ;; (add-hook 'before-save-hook 'gofmt-before-save)
-
- ;; (add-hook 'go-mode-hook (lambda ()
- ;;                           (setq tab-width 4)
- ;;                           (flycheck-add-next-checker 'lsp 'go-vet)
- ;;                           (flycheck-add-next-checker 'lsp 'go-staticcheck)))
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ccls company lsp-mode lsp-ui web-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package eglot
+  :ensure t
+  :config
+  (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
+  (add-hook 'c-ts-mode-hook 'eglot-ensure)
+  (add-hook 'c++-mode-hook 'eglot-ensure)
+  
+  ;; Disable the documentation buffer
+  (setq eglot-extra-doc-mode nil)
+  (add-to-list 'eglot-ignored-server-capabilities :hoverProvider)
+  ;; Optional: disable hover if you don't want any documentation
+  ;; (add-to-list 'eglot-ignored-server-capabilities :hoverProvider)
+  )
